3a356b8ce92eaa8159c2d58b461c554aa20fc0b3
Akshay Gulabrao <agula002@ucr.edu>
2022-02-11 20:54:09 -0800

    added -8? for indel's

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('hello world!')
+import numpy as np
+
+A = "ASDFASDFASDFASDFASFDASFDSADASSDFADSFAASDF"
+B = "ASDFADSFASDFSADFDFSDFASSDASDAFFDSSADFSDAF"
+C = "ASDSDFASDFASFDASDFASDAFSADFSADFDSFSDAFSDF"
+
+def sequence_alignment(a, b):
+    dp = np.zeros((len(a),len(b)))
+    #i indexes A, j indexes B, k indexes C
+    for i in range(len(a)):
+        for j in range(len(b)):
+            if i == 0:
+                dp[i,j] = -4 * j
+            if j == 0:
+                dp[i,j] = -4 * i
+            if A[i] == B[j]:
+                dp[i,j] = max(dp[i-1, j] - 4, dp[i,j-1] - 4,dp[i-1, j-1]+ 5)
+            else:
+                dp[i, j] = max(dp[i - 1, j] - 4, dp[i, j - 1] - 4, dp[i - 1, j - 1] + 5)
+
+
+    print(dp)
+    return 0
+
+sequence_alignment(A,B)
