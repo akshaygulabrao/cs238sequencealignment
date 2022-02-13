de4e1e3fb7dec90b7eeb467ae8d47b3f12b9359e
Akshay Gulabrao <agula002@ucr.edu>
2022-02-12 17:57:31 -0800

    fixed bug in candidate selection

--- main.py
+++ main.py
@@ -19,13 +19,12 @@ def sequence_alignment(a, b):
             if i == 0 or j == 0: continue
             if A[i] == B[j]:
                 #convert to argmaxes and store tuple
-                candidate_moves = [(dp[i-1,j][0] - 8,'left'),(dp[i,j-1][0] - 8,'up'), (dp[i-1,j-1][0]+5, 'upleft')]
+                candidate_moves = [(dp[i-1,j][0] - 8,'up'),(dp[i,j-1][0] - 8,'left'), (dp[i-1,j-1][0]+5, 'upleft')]
                 dp[i,j] = max(candidate_moves,key=lambda x: x[0])
             else:
                 #convert to argmaxes and store tuple
-                candidate_moves = [(dp[i - 1, j][0] - 8, 'left'), (dp[i, j - 1][0] - 8, 'up'), (dp[i - 1, j - 1][0] - 4, 'upleft')]
-                dp[i, j] = max(candidate_moves, key=lambda x: candidate_moves[0])
-
+                candidate_moves = [(dp[i - 1, j][0] - 8, 'up'), (dp[i, j - 1][0] - 8, 'left'), (dp[i - 1, j - 1][0] - 4, 'upleft')]
+                dp[i, j] = max(candidate_moves, key=lambda x: x[0])
 
     print(dp)
     return 0
