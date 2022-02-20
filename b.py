8739f92599dd8f7bbb51317a7b020b183a2fc2c9
Akshay Gulabrao <agula002@ucr.edu>
2022-02-19 21:09:08 -0800

    recursively called path

--- b.py
+++ b.py
@@ -21,7 +21,7 @@ def path(i1,j1,i2,j2):
         S_minus[j1] = 0
         for j in range(j1 + 1, j2+1):
             S_minus[j] = S_minus[j - 1] - 8
-        for i in range(i1 + 1, mid + 1):
+        for i in range(i1 + 1, mid ):
             print(S_minus)
             s = S_minus[j1]
             S_minus[j1] = c = S_minus[j1] - 8
@@ -32,11 +32,12 @@ def path(i1,j1,i2,j2):
                     c = max(S_minus[j] - 8, s - 4, c - 8)
                 s = S_minus[j]
                 S_minus[j] = c
+        print(S_minus)
         print('\n\n')
         S_plus[j2] = 0
         for j in reversed(range(j1,j2)):
             S_plus[j] = S_plus[j+1] - 8
-        for i in reversed(range(mid - 1,i2)):
+        for i in reversed(range(mid - 1,i2+1)):
             print(S_plus)
             s = S_plus[j2]
             S_plus[j2] = c = S_plus[j2] - 8
@@ -47,6 +48,10 @@ def path(i1,j1,i2,j2):
                     c = max(S_plus[j] - 8, s - 4, c - 8)
                 s = S_plus[j]
                 S_plus[j] = c
+        candidate_moves = [S_plus[i] + S_minus[i] for i in range(j1,j2+1)]
+        j = max(range(len(candidate_moves)), key=lambda x:candidate_moves[x])
+        path(i1,j1,mid,j)
+        path(mid,j,i2,j2)
 
 
 
